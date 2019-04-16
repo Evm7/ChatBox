d4b7dc38fc57fd9ba8369d731b62ef7d3bc98ca4
Evm7 <estevevm.7@gmail.com>
2019-04-16 21:31:54 +0200

    Update

---
+++ README.md
@@ -0,0 +1,28 @@
+ChatBox
+==============
+
+This is ChatBox.
+ChatBox is an applicaction client-server which runs in port 9080 and creates a connection between multiple clients to one uniq server.
+Most important aspect is that it let users to enter an specific group (or room) and messages are only transmitted to this particular room.
+
+
+We have base our proect in some applications find on-line and the Udemy Course Doccumentation: "The Complete Node js"
+
+Usage
+-----
+
+Here is how you use it:
+
+ * Enter to server directory and use command: node server.js
+ * Connect to the browser and seach for the url: http://localhost:9080
+ * Enter your username and the room you want to chat to
+ * Now you are able to interact with all the others users connected to the same chat Room.
+
+
+Libraries used:
+
+- [socket.io](https://github.com/socketio/socket.io)
+- [jQuery](https://github.com/jquery/jquery)
+- [express](https://github.com/expressjs/express)
+- [mustache.js](https://github.com/janl/mustache.js)
+- [Moment.js](https://github.com/moment/moment)
---
+++ public/js/chat.js
@@ -0,0 +1,82 @@
+/* jshint node: true */
+/* jshint esversion: 6 */
+/* globals $:false*/
+const socket = io();
+var username;
+var users_room;
+const colours = ['crimson', 'mediumslateblue', 'fuchsia', 'blue', 'yellowgreen', 'darkmagenta', 'darkturquoise', 'lawngreen', 'orangered', 'goldenrod'];
+
+function scrollToBottom() {
+    const messages = jQuery('#messages');
+    const newMessage = messages.children('li:last-child');
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+socket.on('connect', function () {
+    const params = jQuery.deparam(window.location.search);
+    username = params.username;
+    socket.emit('join', params, function (err) {
+        if (err) {
+            alert(err);
+            window.location.href = '/';
+        }
+        else {
+            console.log('No error');
+        }
+    });
+});
+socket.on('disconnect', function () {
+    console.log('Disconnected from the server');
+});
+socket.on('updateUserList', function (users) {
+    users_room = users;
+    const ol = jQuery('<ol id="colores"></ol>');
+    users.forEach(function (user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+    jQuery(document).ready(function () {
+        jQuery('#colores li').each(function (i) {
+            $(this).css("color", colours[i % colours.length]);
+        }).hover(function (e) {
+            $(this).css('opacity', 0.7);
+        }, function (e) {
+            $(this).css('opacity', 1);
+        });
+    });
+    jQuery('#users').html(ol);
+});
+socket.on('newMessage', function (message) {
+    const formattedTime = moment(message.createdAt).format('H:mm ');
+    var template;
+    //if I send the message
+    if (username === message.from) {
+        template = jQuery('#message-template-alter').html();
+    }
+    //if another user send the message
+    else {
+        template = jQuery('#message-template').html();
+    }
+    const html = Mustache.render(template, {
+        colour: colours[users_room.indexOf(message.from) % colours.length]
+        , text: message.text
+        , from: message.from
+        , createdAt: formattedTime
+    , });
+    jQuery('#messages').append(html);
+    scrollToBottom();
+});
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    const messageTextBox = jQuery('[name=message]');
+    socket.emit('createMessage', {
+        text: messageTextBox.val()
+    }, function () {
+        messageTextBox.val('');
+    });
+});
